9d55d7a27a2f9e85bbe13b789fb169d7653a893a
Matthieu Dufour <mdufourpro@gmail.com>
2018-01-31 21:42:31 +0100

    Slight changes in file_loader.py module
    - Change logging messages (colons added)
    - Ensure returned "config" is a Python list

--- codestacker/file_loader.py
+++ codestacker/file_loader.py
@@ -9,7 +9,7 @@ File loading utility.
 
 def load_yaml(file):
     """
-    Read a YAML file and return its content as a dictionary--or die.
+    Read a YAML file and return its content as a list of dictionaries--or die.
     """
     import yaml
 
@@ -21,11 +21,14 @@ def load_yaml(file):
         with open(file, 'r') as stream:
             config = list(yaml.safe_load_all(stream))
     except IOError as error:
-        print_and_die('File I/O', error)
+        print_and_die('File I/O:', error)
     except yaml.YAMLError as error:
-        print_and_die('YAML parsing error', error)
+        print_and_die('YAML parsing error:', error)
 
     if config is None:
-        print_and_die('Empty configuration file')
+        print_and_die('Empty configuration file!')
+
+    if isinstance(config, dict):
+        config = [config]
 
     return config
