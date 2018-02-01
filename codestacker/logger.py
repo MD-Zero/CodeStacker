5a9ab92a6a17e662381b3861fa02ad77e44b15d8
Matthieu Dufour <mdufourpro@gmail.com>
2018-02-01 21:40:12 +0100

    Enrich logger.py module
    - Add "log_ok" and "log_ko" functions
    - Implement "public" / "private" semantics

--- codestacker/logger.py
+++ codestacker/logger.py
@@ -5,30 +5,17 @@
 Logging utilities.
 """
 
-LOG_INFO = '[INFO ]'
-LOG_WARNING = '[WARN ]'
-LOG_ERROR = '[ERROR]'
+_LOG_ERROR = '[ERROR]'
+_LOG_INFO = '[INFO ]'
+_LOG_KO = '[ KO  ]'
+_LOG_OK = '[ OK  ]'
+_LOG_WARNING = '[WARN ]'
 
-COL_BLUE = '\033[94m'
-COL_NONE = '\033[0m'
-COL_RED = '\033[91m'
-COL_YELLOW = '\033[93m'
-
-####################################################################################################
-
-def log_info(message):
-    """
-    Log an information message in the terminal.
-    """
-    print('{}{} {}{}'.format(COL_BLUE, LOG_INFO, message, COL_NONE))
-
-####################################################################################################
-
-def log_warning(message):
-    """
-    Log a warning message in the terminal.
-    """
-    print('{}{} {}{}'.format(COL_YELLOW, LOG_WARNING, message, COL_NONE))
+_COL_BLUE = '\033[94m'
+_COL_GREEN = '\033[92m'
+_COL_NONE = '\033[0m'
+_COL_RED = '\033[91m'
+_COL_YELLOW = '\033[93m'
 
 ####################################################################################################
 
@@ -36,4 +23,36 @@ def log_error(message):
     """
     Log an error message in the terminal.
     """
-    print('{}{} {}{}'.format(COL_RED, LOG_ERROR, message, COL_NONE))
+    print('{}{} {}{}'.format(_COL_RED, _LOG_ERROR, message, _COL_NONE))
+
+####################################################################################################
+
+def log_info(message):
+    """
+    Log an information message in the terminal.
+    """
+    print('{}{} {}{}'.format(_COL_BLUE, _LOG_INFO, message, _COL_NONE))
+
+####################################################################################################
+
+def log_ko(message):
+    """
+    Log a "KO" message in the terminal.
+    """
+    print('{}{} {}{}'.format(_COL_RED, _LOG_KO, message, _COL_NONE))
+
+####################################################################################################
+
+def log_ok(message):
+    """
+    Log an "OK" message in the terminal.
+    """
+    print('{}{} {}{}'.format(_COL_GREEN, _LOG_OK, message, _COL_NONE))
+
+####################################################################################################
+
+def log_warning(message):
+    """
+    Log a warning message in the terminal.
+    """
+    print('{}{} {}{}'.format(_COL_YELLOW, _LOG_WARNING, message, _COL_NONE))
