e322ff1fd10b704cda3c7a254fe7085d0e38fb9d
Matthieu Dufour <mdufourpro@gmail.com>
2018-02-04 18:30:12 +0100

    Update constants.py module
    - Add new configuration keys
    - Remove option keys

--- codestacker/constants.py
+++ codestacker/constants.py
@@ -7,12 +7,9 @@ Constants.
 
 # Configuration "keys".
 KEY_COMPILER_OPTIONS = 'compiler_options'
+KEY_DIR_BIN = 'dir_bin'
 KEY_DIR_BUILD = 'dir_build'
 KEY_DIR_INCLUDE = 'dir_include'
 KEY_DIR_SOURCE = 'dir_source'
+KEY_LIBRARIES = 'libraries'
 KEY_OUTPUT = 'output'
-KEY_PROFILE = 'profile'
-
-# Options.
-OPT_FILE = 'file'
-OPT_PROFILE = 'profile'
