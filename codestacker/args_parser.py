80f29539281ecd254ce59c1b906e729d9abcf430
Matthieu Dufour <mdufourpro@gmail.com>
2018-02-14 23:49:10 +0100

    Reorganize command line arguments processing

--- codestacker/args_parser.py
+++ codestacker/args_parser.py
@@ -15,19 +15,25 @@ def parse_args():
     import sys
 
     parser = argparse.ArgumentParser(prog='codestacker')
+    sub_parser = parser.add_subparsers(dest='command')
 
-    # 'build' arguments.
-    parser_build = parser.add_subparsers().add_parser(
-        'build',
-        help='trigger the build process')
-
-    parser_build.add_argument(
+    parser.add_argument(
         '-f', '--file',
         default='blueprint.yaml', help='specify the blueprint file to use')
 
-    parser_build.add_argument(
+    parser.add_argument(
         '-c', '--config',
         default='default', help='specify the configuration to use')
+
+    # 'clean' argument.
+    sub_parser.add_parser(
+        'clean',
+        help='clean the compilation results')
+
+    # 'build' argument.
+    sub_parser.add_parser(
+        'build',
+        help='trigger the build process')
 
     # Parse the arguments.
     # May abort the script if unexpected arguments were passed.
