161020e2c8ff675e9df2b7ac85d1c39f2b5b207c
Matthieu Dufour <mdufourpro@gmail.com>
2018-02-22 21:06:40 +0100

    Add method in file_loader.py module to dump YAML content into file

--- codestacker/file_loader.py
+++ codestacker/file_loader.py
@@ -7,6 +7,10 @@ File loading utility.
 
 ####################################################################################################
 
+_ERROR_FILE_READING = 'file reading error'
+_ERROR_YAML_PARSING = 'YAML parsing error'
+_ERROR_EMPTY_FILE = 'empty YAML file'
+
 def load_yaml(file):
     """
     Read a YAML file and return its content as a list of dictionaries.
@@ -26,12 +30,12 @@ def load_yaml(file):
         with open(file, 'r') as stream:
             content = list(yaml.safe_load_all(stream))
     except IOError as error:
-        raise TechnicalError('file handling error', error)
+        raise TechnicalError(_ERROR_FILE_READING, error)
     except yaml.YAMLError as error:
-        raise TechnicalError('YAML parsing error', error)
+        raise TechnicalError(_ERROR_YAML_PARSING, error)
 
     if not content:
-        raise TechnicalError('empty YAML file')
+        raise TechnicalError(_ERROR_EMPTY_FILE)
 
     # Transform the "list of dictionaries" into one single dictionary.
     content = {key: value for pair in content for key, value in pair.items()}
@@ -39,3 +43,24 @@ def load_yaml(file):
     log_ok('<< Success')
 
     return content
+
+####################################################################################################
+
+_ERROR_FILE_WRITING = 'file writing error'
+_ERROR_YAML_DUMPING = 'YAML dumping error'
+
+def dump_yaml(content, file):
+    """
+    Dump some YAML content into a file.
+    """
+    import yaml
+
+    from .exceptions import TechnicalError
+
+    try:
+        with open(file, 'w') as stream:
+            yaml.safe_dump(content, stream)
+    except IOError as error:
+        TechnicalError(_ERROR_FILE_WRITING, error).print()
+    except yaml.YAMLError as error:
+        TechnicalError(_ERROR_YAML_DUMPING, error).print()
