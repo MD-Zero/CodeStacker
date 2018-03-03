b254343c0a10d0038595ed266e7cf69fb567f68e
Matthieu Dufour <mdufourpro@gmail.com>
2018-03-03 22:09:12 +0100

    Improve code re-usage in sources_inspector.py module

--- codestacker/sources_inspector.py
+++ codestacker/sources_inspector.py
@@ -7,33 +7,35 @@ Sources inspector module: validate the correctness of source files.
 
 ####################################################################################################
 
-_ERROR_INVALID_FILE = 'file "{}" doesn\'t match filenames requirements'
-
 def validate_sources(include_dir, sources_dir):
     """
-    Check if headers and sources filename is a valid one.
+    Check if headers and sources filenames are valid.
+    """
+    from .logger import Logger
+
+    Logger.begin('Checking headers and sources...')
+
+    _check_directory(include_dir, '.hpp')
+    _check_directory(sources_dir, '.cpp')
+
+    Logger.end('Headers and sources valid')
+
+####################################################################################################
+
+_ERROR_INVALID_FILE = 'file "{}" doesn\'t match filenames requirements'
+
+def _check_directory(directory, file_extension):
+    """
+    Check the directory's content, for all files ending with "file_extension".
     """
     import os
     import re
 
     from .exceptions  import TechnicalError
     from .file_system import get_files
-    from .logger      import Logger
 
-    Logger.begin('Checking headers and sources...')
-
-    # Headers.
-    for file in get_files(include_dir, '.hpp'):
+    for file in get_files(directory, file_extension):
         filename = os.path.basename(file)
 
-        if re.search(r'^\w+\.hpp$', filename) is None:
+        if re.search(r'^\w+{}$'.format(file_extension), filename) is None:
             raise TechnicalError(_ERROR_INVALID_FILE.format(filename))
-
-    # Sources.
-    for file in get_files(include_dir, '.cpp'):
-        filename = os.path.basename(file)
-
-        if re.search(r'^\w+\.cpp$', filename) is None:
-            raise TechnicalError(_ERROR_INVALID_FILE.format(filename))
-
-    Logger.end('Headers and sources valid')
