8d2bba5d77dbb78e66147aa53a094f296b0ba68b
Matthieu Dufour <mdufourpro@gmail.com>
2018-03-06 22:49:23 +0100

    Alter test source files to integrate libraries support

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -1,13 +1,39 @@
 #include <iostream>
 
+#include <GLFW/glfw3.h>
+
 #include "Foo/Foo.hpp"
 #include "Bar/Bar.hpp"
 
 int main()
 {
-    foo::Foo foo{};
+    foo::Foo foo {};
+    bar::Bar bar {};
 
     foo.doNothing();
+    bar.doNothing();
+
+    GLFWwindow * window { nullptr };
+
+    if (!glfwInit()) return -1;
+
+    window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
+
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
 
     return 0;
 }
